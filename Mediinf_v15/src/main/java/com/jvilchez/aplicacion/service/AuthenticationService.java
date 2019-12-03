9780d863b28d2f87cf6d817069092a27df13f8df
JuniorTecsup <54334297+JuniorTecsup@users.noreply.github.com>
2019-12-03 16:17:59 -0500

    v15

---
+++ Mediinf_v15/src/main/java/com/jvilchez/aplicacion/service/AuthenticationService.java
@@ -0,0 +1,31 @@
+package com.jvilchez.aplicacion.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.stereotype.Service;
+
+import com.jvilchez.aplicacion.entity.User;
+import com.jvilchez.aplicacion.repository.AuthenticationRepository;
+
+@Service
+public class AuthenticationService {
+
+private static final Logger logger = LoggerFactory.getLogger(AuthenticationService.class);
+	
+	@Autowired
+    private AuthenticationRepository authenticationRepository;
+	
+	public User login(String correo, String clave) throws BadCredentialsException {
+		//logger.info("login("+correo+", "+clave+")");
+        return authenticationRepository.login(correo, clave);
+    }
+	
+	public User findByCorreo(String correo) throws EmptyResultDataAccessException{
+		//logger.info("findByCorreo("+correo+")");
+		return authenticationRepository.findByCorreo(correo);
+	}
+
+}
